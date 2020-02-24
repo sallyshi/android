a01bbe10a8e7823d265ddea853f45cd319812793
sally shi <sally.l.shi@gmail.com>
2020-02-23 16:05:29 -0800

    json complete

--- app/src/main/java/com/sallylshi/touchstonecalendar/MainActivity.java
+++ app/src/main/java/com/sallylshi/touchstonecalendar/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.JsonReader;
+import android.util.JsonToken;
+import android.util.Log;
 import android.widget.TextView;
 import java.io.IOException;
 import java.io.StringReader;
@@ -30,65 +32,83 @@ public class MainActivity extends AppCompatActivity {
                     String output = "";
                     Document doc = Jsoup.connect(MISSION_CLIFFS_URL).get();
                     Element element = doc.select("script[id*=\"timely-calendar-state\"]").first();
-                    JsonReader reader = new JsonReader(new StringReader(element.data()));
 
-                    while(reader.hasNext()) {
-                        switch (reader.peek()) {
-                            case BEGIN_ARRAY: {
-                                reader.beginArray();
-                                output += "[\n";
-                                break;
-                            }
-                            case BEGIN_OBJECT: {
-                                reader.beginObject();
-                                output += "{\n";
-                                break;
-                            }
-                            case BOOLEAN: {
-                                output += reader.nextBoolean() + "\n";
-                                break;
-                            }
-                            case END_ARRAY: {
-                                reader.endArray();
-                                output += "]\n";
-                                break;
-                            }
-                            case END_DOCUMENT: {
-                                reader.close();
-                                break;
-                            }
-                            case END_OBJECT: {
-                                reader.endObject();
-                                output += "}\n";
-                                break;
-                            }
-                            case NAME: {
-                                output += "\"" + reader.nextName() + "\": ";
-                                break;
-                            }
-                            case NULL: {
-                                reader.nextNull();
-                                output += "null,\n";
-                            }
-                            case NUMBER: {
-                                output += reader.nextDouble() + ",\n";
-                                break;
-                            }
-                            case STRING: {
-                            output += reader.nextString() + ",\n";
-                                break;
-                            }
-                        }
-                    }
+                    TextView view = findViewById(R.id.test);
+
+                    String json = element.data();
+                    final String realjson = json.replaceAll("&q;", "\"");
+
+//                    runOnUiThread( () ->view.setText(realjson));
+
+                    JsonReader reader = new JsonReader(new StringReader(realjson));
+                    reader.setLenient(true);
+
+
+                    final String thisIsreallytheoutput = parse(reader);
 
                     // Test on UI
-                    TextView view = findViewById(R.id.test);
-                    runOnUiThread( () ->view.setText(element.data()));
+                    runOnUiThread( () ->view.setText(thisIsreallytheoutput));
+
 
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }).start();
+    }
+
+    public String parse(JsonReader reader) throws IOException{
+        String output = "";
+        while(reader.peek() == JsonToken.END_ARRAY || reader.peek() == JsonToken.END_OBJECT || reader.hasNext()) {
+            Log.i("parse", "" + reader.peek());
+            switch (reader.peek()) {
+                case BEGIN_ARRAY: {
+                    reader.beginArray();
+                    output += "[\n";
+                    break;
+                }
+                case BEGIN_OBJECT: {
+                    reader.beginObject();
+                    output += "{\n";
+                    break;
+                }
+                case BOOLEAN: {
+                    output += reader.nextBoolean() + "\n";
+                    break;
+                }
+                case END_ARRAY: {
+                    reader.endArray();
+                    output += "]\n";
+                    break;
+                }
+                case END_DOCUMENT: {
+                    reader.close();
+                    return output;
+                }
+                case END_OBJECT: {
+                    reader.endObject();
+                    output += "}\n";
+                    break;
+                }
+                case NAME: {
+                    output += "\"" + reader.nextName() + "\": ";
+                    break;
+                }
+                case NULL: {
+                    reader.nextNull();
+                    output += "null,\n";
+                    break;
+                }
+                case NUMBER: {
+                    output += reader.nextDouble() + ",\n";
+                    break;
+                }
+                case STRING: {
+                    output += reader.nextString() + ",\n";
+                    break;
+                }
+            }
+        }
+        return output;
     }
 }
